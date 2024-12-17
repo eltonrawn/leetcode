e0e4c52278f49da6817a74bc165b66dd3e94ed30
rawnak <eltonrawn@gmail.com>
2024-12-17 07:27:41 +0700

    Add 2762 with linear complexity

--- src/solutions/kotlin/2762_continuousSubarray.kt
+++ src/solutions/kotlin/2762_continuousSubarray.kt
@@ -1,6 +1,7 @@
 package solutions.kotlin
 
 import kotlin.math.abs
+import java.util.Deque
 
 class `2762_continuousSubarray` {
     fun continuousSubarrays(nums: IntArray): Long {
@@ -27,9 +28,50 @@ class `2762_continuousSubarray` {
         }
         return ans
     }
+
+    fun continuousSubarraysDeque(nums: IntArray): Long {
+        var le = 0
+        var ri = 0
+        var diff = 0
+        val mxQueue = ArrayDeque<Pair<Int, Int>>()
+        val mnQueue = ArrayDeque<Pair<Int, Int>>()
+        var ans = 0L
+        while(le < nums.size) {
+            // increase the right pointer till it's ok
+            while(ri < nums.size) {
+                if(abs(maxOf(nums[ri], if(mxQueue.isNotEmpty()) (mxQueue.first().first) else 0) -
+                            minOf(nums[ri], if(mnQueue.isNotEmpty()) (mnQueue.first().first) else 1000000001)) > 2) {
+                    break
+                }
+
+                while(mxQueue.isNotEmpty() && mxQueue.last().first <= nums[ri]) {
+                    mxQueue.removeLast()
+                }
+                mxQueue.addLast(Pair(nums[ri], ri))
+
+                while(mnQueue.isNotEmpty() && mnQueue.last().first >= nums[ri]) {
+                    mnQueue.removeLast()
+                }
+                mnQueue.addLast(Pair(nums[ri], ri))
+
+                ri++
+            }
+
+            // increase left pointer once and get ans
+            ans += (ri - le)
+            if(mxQueue.first().second == le) {
+                mxQueue.removeFirst()
+            }
+            if(mnQueue.first().second == le) {
+                mnQueue.removeFirst()
+            }
+            le++
+        }
+        return ans
+    }
 }
 
 /**
- * tag: medium, revise, two pointer
+ * tag: medium, revise, two pointer, revise for O(n), deque, monotonic queue
  *
  * */
