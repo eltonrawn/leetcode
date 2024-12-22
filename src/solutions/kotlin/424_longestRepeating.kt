3977842aba26094a1c577c826af65d56aaed87d3
rawnak <eltonrawn@gmail.com>
2024-12-22 11:48:13 +0700

    Add 424

---
+++ src/solutions/kotlin/424_longestRepeating.kt
@@ -0,0 +1,37 @@
+package solutions.kotlin
+
+class `424_longestRepeating` {
+    fun characterReplacement(s: String, k: Int): Int {
+        var ans = 0
+        for(ch in 'A' .. 'Z') {
+            var le = 0
+            var ri = 0
+            var kRemain = k
+            while(le < s.length) {
+                // increase ri until it doesn't fulfill
+                while((ri < s.length) && (s[ri] == ch || kRemain > 0)) {
+                    if(s[ri] != ch) {
+                        kRemain--
+                    }
+                    ri++
+                }
+                ans = maxOf(ans, ri - le)
+
+                // increase le as we already have best answer including it
+                if(s[le] != ch && le < ri) {
+                    kRemain++
+                }
+                le++
+                while(le > ri) {
+                    ri++
+                }
+            }
+        }
+        return ans
+    }
+}
+
+/**
+ * tag: medium, revise, sliding window, two pointer
+ *
+ * */
