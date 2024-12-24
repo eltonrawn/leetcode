fbc86d57262cbff1d27f53858ba144d255931472
rawnak <eltonrawn@gmail.com>
2024-12-25 01:06:33 +0700

    Add 160

---
+++ src/solutions/kotlin/160_intersectionOfTwo.kt
@@ -0,0 +1,54 @@
+package solutions.kotlin
+
+class `160_intersectionOfTwo` {
+
+    class ListNode {
+        val next: ListNode? = null
+    }
+
+    fun moveForward(cur: ListNode?, cntOfForward: Int): ListNode? {
+        if(cntOfForward == 0) {
+            return cur
+        }
+        return moveForward(cur?.next, cntOfForward - 1)
+    }
+
+    fun getIntersectionNode(headA:ListNode?, headB:ListNode?):ListNode? {
+        var curA = headA
+        var curB = headB
+
+        var aForward = 0
+        var bForward = 0
+        while(curA != null || curB != null) {
+            var isBreak = 0
+            if(curA != null) {
+                curA = curA.next
+            }
+            else {
+                bForward++
+            }
+            if(curB != null) {
+                curB = curB.next
+            } else {
+                aForward++
+            }
+        }
+
+        println("aForward: $aForward")
+        println("bForward: $bForward")
+
+        var newHeadA = moveForward(headA, aForward)
+        var newHeadB = moveForward(headB, bForward)
+
+
+        curA = newHeadA
+        curB = newHeadB
+
+        while(curA != curB) {
+            curA = curA?.next
+            curB = curB?.next
+        }
+        return curA
+
+    }
+}
